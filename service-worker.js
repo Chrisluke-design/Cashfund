abe9edc59ba23caf597828159f85c3917f110cab
Chrisluke-design <mbaefosupply@gmail.com>
2024-12-10 07:03:13 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,36 @@
-
-const CACHE_NAME = 'my-pwa-cache-v1';
+const CACHE_NAME = 'billionairehk-cache-v1';
 const urlsToCache = [
     '/',
     '/index.html',
+    '/billionairehk1.png',
     '/icon-192x192.png',
-    '/icon-512x512.png'
+    '/icon-512x512.png',
 ];
 
-// Install event
+// Install Service Worker
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(cache => {
+        caches.open(CACHE_NAME).then(cache => {
             console.log('Opened cache');
             return cache.addAll(urlsToCache);
         })
     );
 });
 
-// Fetch event
+// Fetch Resources
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-        .then(response => {
-            // Cache hit - return response
-            if (response) {
-                return response;
-            }
-            return fetch(event.request);
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
         })
     );
 });
 
-// Activate event
+// Activate Service Worker
 self.addEventListener('activate', event => {
     const cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
-        caches.keys()
-        .then(cacheNames => {
+        caches.keys().then(cacheNames => {
             return Promise.all(
                 cacheNames.map(cacheName => {
                     if (!cacheWhitelist.includes(cacheName)) {
